0e5b8e28f09f00aa6aae855c47f0e557894d4c16
SLotreck <lotrecks@msu.edu>
2023-02-07 15:53:05 -0500

    Write draft of all code to obtain and format gpt3 outputs

---
+++ tests/gpt3_openai_test.py
@@ -0,0 +1,13 @@
+"""
+Tests for the output-formatting portion of gpt3_openai.py.
+
+Author: Serena G. Lotreck
+"""
+import pytest
+import sys
+sys.path.append('../benchmark_scripts/')
+from gpt3_openai import format_dygiepp_doc, find_sent_idx
+
+
+
+
