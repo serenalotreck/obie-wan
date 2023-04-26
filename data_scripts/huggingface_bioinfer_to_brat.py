09cd2e3ab3fabe5886e4487d94ddbd8074378fee
SLotreck <lotrecks@msu.edu>
2023-04-26 12:04:37 -0400

    Finish and debug huggingface2brat code and tests

--- data_scripts/huggingface_bioinfer_to_brat.py
+++ data_scripts/huggingface_bioinfer_to_brat.py
@@ -9,6 +9,194 @@ Author: Serena G. Lotreck
 import argparse
 from os.path import abspath
 from datasets import load_dataset
+from collections import defaultdict
+from tqdm import tqdm
+
+
+def replace_ids(ent_anns, rel_anns):
+    """
+    Removes the original annotation ID's and joins annotations into a formatted
+    string for saving.
+
+    parameters:
+        ent_anns, dict: keys are original ent ID's, values are ent anns
+        rel_anns, dict: keys are original rel ID's, values are rel anns
+
+    returns:
+        anns_to_save, str: formatted string
+    """
+    ann_list = []
+    for ent_key, ent in ent_anns.items():
+        ann_list.append(ent)
+    for rel_key, rel in rel_anns.items():
+        ann_list.append(rel)
+    anns_to_save = '\n'.join(ann_list)
+
+    return anns_to_save
+
+
+def get_joined_text(docs):
+    """
+    Gets the full joined text of a document.
+
+    parameters:
+        docs, list of dict: doc objects to join
+
+    returns:
+        text, str: full doc text
+    """
+    text = ''
+    for doc in docs:
+        if text != '':
+            text += ' ' + doc['text']
+        else:
+            text += doc['text']
+
+    return text
+
+
+def get_rel_anns(docs, ent_anns):
+    """
+    Map relations onto renamed entities and format for brat.
+
+    parameters:
+        docs, list of dict: docs form which to get relations
+        ent_anns, dict: keys are original entity ID's, values are brat entities
+
+    returns:
+        rel_anns, dict: keys are old rel ID's (for consistency with
+            get_ent_anns), and values are brat-formatted relations
+    """
+    rel_anns = {}
+    r_num = 1
+    for doc in docs:
+        for rel in doc['relations']:
+            rel_key = rel['id']
+            r_key = f'R{r_num}'
+            rel_type = rel['type']
+            arg1_orig_id = rel['arg1_id']
+            arg2_orig_id = rel['arg2_id']
+            arg1_tid = ent_anns[arg1_orig_id].split('\t')[0]
+            arg2_tid = ent_anns[arg2_orig_id].split('\t')[0]
+            full_ann = f'{r_key}\t{rel_type} Arg1:{arg1_tid} Arg2:{arg2_tid}'
+            rel_anns[rel_key] = full_ann
+            r_num += 1
+
+    return rel_anns
+
+
+def get_ent_anns(docs):
+    """
+    Format entity annotations into brat format, but leave original entity ID as
+    the key of the output dictionary for alter mapping onto relations.
+
+    parameters:
+        docs, list of dict: docs from which to get entities
+
+    returns:
+        ent_anns, dict: keys are original entity ID's, values are brat strings
+    """
+    ent_anns = {}
+    t_num = 1
+    prev_len = 0
+    for doc in docs:
+        for ent in doc['entities']:
+            ent_key = ent['id']
+            t_key = f'T{t_num}'
+            ent_type = ent['type']
+            # Check for cases where there's more than one offset list
+            assert len(ent['offsets']) == 1, f'ent: {ent}'
+            start = ent['offsets'][0][0] + prev_len
+            end = ent['offsets'][0][1] + prev_len
+            # Check that there's only one text
+            assert len(ent['text']) == 1, f'ent: {ent}'
+            txt = ent['text'][0]
+            full_ann = f'{t_key}\t{ent_type} {start} {end}\t{txt}'
+            ent_anns[ent_key] = full_ann
+            t_num += 1
+        prev_len += len(doc['text']) + 1 # For the space
+
+    return ent_anns
+
+
+def join_docs(dset):
+    """
+    Indexes the dataset by document number, not sentence, and joins sentences
+    from the same doc into one list.
+
+    parameters:
+        dset, Huggingface Dataset instance: dataset to join
+
+    returns:
+        joined_dset, dict: values are doc ID's and keys are lists of doc
+            dictionaries
+    """
+    joined_dset = defaultdict(list)
+    for item in dset:
+        # Make sure they're all sentences
+        assert item['type'] == 'Sentence'
+        # Make sure the doc ID splits correctly
+        assert len(item['document_id'].split('.')) == 3
+        # Get shortened doc ID
+        new_id = '.'.join(item['document_id'].split('.')[:2])
+        # Add to joined list
+        joined_dset[new_id].append(item)
+
+    return joined_dset
+
+
+def huggingface2brat(dataset, out_loc, out_prefix):
+    """
+    Convert the huggingface bioinfer dataset to brat format.
+
+    parameters:
+        dataset, Huggingface Dataset instance: dataset to convert
+        out_loc, str: directory to save output
+        out_prefix, str: prefix, if any, to prepend to file names
+
+    returns: None
+    """
+    # Join sentences from the same abstract into the same document
+    joined_dset = join_docs(dataset)
+
+    # Get the docs to save
+    for doc_key, docs in tqdm(joined_dset.items()):
+        # Get the annotations in brat format but leave original ID's for now
+        ent_anns = get_ent_anns(docs)
+        rel_anns = get_rel_anns(docs, ent_anns)
+        # Get text
+        text = get_joined_text(docs)
+        # Replace original ID's with sequential brat standoff ID's
+        anns_to_save = replace_ids(ent_anns, rel_anns)
+        # Get save names
+        txt_save = f'{out_loc}/{out_prefix}{doc_key}.txt'
+        ann_save = f'{out_loc}/{out_prefix}{doc_key}.ann'
+        # Save docs
+        with open(txt_save, 'w') as myf:
+            myf.write(text)
+        with open(ann_save, 'w') as myf:
+            myf.write(anns_to_save)
+
+
+def main(out_loc, out_prefix):
+
+    # Check existence of out_loc
+    verboseprint('\nChecking existence of output directory...')
+    if not exists(out_loc):
+        mkdir(out_loc)
+        verboseprint(f'New directory created at {out_loc}')
+
+    # Read in both datasets
+    verboseprint('\nReading in datasets...')
+    train = load_dataset('bigbio/bioinfer', split='train')
+    test = load_dataset('bigbio/bioinfer', split='test')
+
+    # Convert to brat
+    verboseprint('\nConverting datasets...')
+    huggingface2brat(train, out_loc, out_prefix)
+    huggingface2brat(test, out_loc, out_prefix)
+
+    verboseprint('\nDone!')
 
 
 if __name__ == "__main__":
@@ -16,9 +204,18 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Convert BioInfer')
 
     parser.add_argument('out_loc', type=str,
-            help='Path to directory to save output')
+            help='Path to directory to save output. Will be created if it '
+            'doesn\'t already exist')
     parser.add_argument('-out_prefix', type=str, default='',
             help='Prefix to prepend to output files')
     parser.add_argument('--verbose', '-v', action='store_true',
             help='Whether or not to print updates')
+
+    args = parser.parse_args()
+
+    args.out_loc = abspath(args.out_loc)
+
+    verboseprint = print if args.verbose else lambda *a, **k: None
+
+    main(args.out_loc, args.out_prefix)
 
---
+++ tests/huggingface_bioinfer_to_brat_test.py
@@ -0,0 +1,425 @@
+"""
+Spot checks for converting bioinfer huggingface format to brat.
+
+Author: Serena G. Lotreck
+"""
+import pytest
+import sys
+sys.path.append('../data_scripts')
+import huggingface_bioinfer_to_brat as hbb
+
+
+@pytest.fixture
+def rep_ids_ent_input():
+    return {'origid1': 'T1\tProtein 0 3\tFLO1',
+            'origid2': 'T2\tDNA 7 9\tpro'}
+
+@pytest.fixture
+def rep_ids_rel_input():
+    return {'relorig1': 'R1\tProduces Arg1:T2 Arg2:T1'}
+
+@pytest.fixture
+def rep_ids_output():
+    return ('T1\tProtein 0 3\tFLO1\nT2\tDNA 7 9\tpro\n'
+            'R1\tProduces Arg1:T2 Arg2:T1')
+
+def test_replace_ids(rep_ids_ent_input, rep_ids_rel_input, rep_ids_output):
+
+    anns_to_save = hbb.replace_ids(rep_ids_ent_input, rep_ids_rel_input)
+
+    assert anns_to_save == rep_ids_output
+
+
+@pytest.fixture
+def joined_txt_input_one_sent():
+    return [{'document_id': 'doc1', 'text': 'This is a sentence.'}]
+
+@pytest.fixture
+def joined_txt_output_one_sent():
+    return 'This is a sentence.'
+
+def test_get_joined_text_one_sent(joined_txt_input_one_sent,
+        joined_txt_output_one_sent):
+
+    text = hbb.get_joined_text(joined_txt_input_one_sent)
+
+    assert text == joined_txt_output_one_sent
+
+
+@pytest.fixture
+def joined_txt_input_mult_sent():
+    return [{'document_id': 'doc1', 'text': 'This is a sentence.'},
+            {'document_id': 'doc2', 'text': 'This is another sentence.'},
+            {'document_id': 'doc3', 'text': 'And this is another.'}]
+
+@pytest.fixture
+def joined_txt_output_mult_sent():
+    return 'This is a sentence. This is another sentence. And this is another.'
+
+def test_get_joined_text_mult_sent(joined_txt_input_mult_sent,
+        joined_txt_output_mult_sent):
+
+    text = hbb.get_joined_text(joined_txt_input_mult_sent)
+
+    assert text == joined_txt_output_mult_sent
+
+
+@pytest.fixture
+def ent_anns_one_doc_input():
+    return [{'document_id': 'BioInfer.d0.s0', 'type': 'Sentence',
+            'text': 'alpha-catenin inhibits beta-catenin signaling by '
+            'preventing formation of a beta-catenin*T-cell factor*DNA '
+            'complex.',
+            'entities':[{'id': 'BioInfer.d0.s0.e0', 'offsets': [[88, 101]],
+                'text': ['T-cell factor'], 'type': 'Individual_protein',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.e1', 'offsets': [[0, 13]],
+                'text': ['alpha-catenin'], 'type': 'Individual_protein',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.e2', 'offsets': [[23, 35]],
+                'text': ['beta-catenin'], 'type': 'Individual_protein',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.e3', 'offsets': [[75, 87]],
+                'text': ['beta-catenin'], 'type': 'Individual_protein',
+                'normalized': []}]}]
+
+@pytest.fixture
+def ent_anns_one_doc_output():
+    return {
+            'BioInfer.d0.s0.e0': 'T1\tIndividual_protein 88 101\tT-cell factor',
+            'BioInfer.d0.s0.e1': 'T2\tIndividual_protein 0 13\talpha-catenin',
+            'BioInfer.d0.s0.e2': 'T3\tIndividual_protein 23 35\tbeta-catenin',
+            'BioInfer.d0.s0.e3': 'T4\tIndividual_protein 75 87\tbeta-catenin'
+            }
+
+def test_get_ent_anns_one_doc(ent_anns_one_doc_input, ent_anns_one_doc_output):
+
+    ent_anns = hbb.get_ent_anns(ent_anns_one_doc_input)
+
+    assert ent_anns == ent_anns_one_doc_output
+
+
+@pytest.fixture
+def ent_anns_mult_doc_input():
+    return [{'document_id': 'BioInfer.d0.s0', 'type': 'Sentence',
+            'text': 'alpha-catenin inhibits beta-catenin signaling by '
+            'preventing formation of a beta-catenin*T-cell factor*DNA '
+            'complex.',
+            'entities': [{'id': 'BioInfer.d0.s0.e0', 'offsets': [[88, 101]],
+                'text': ['T-cell factor'], 'type': 'Individual_protein',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.e1', 'offsets': [[0, 13]],
+                'text': ['alpha-catenin'], 'type': 'Individual_protein',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.e2', 'offsets': [[23, 35]],
+                'text': ['beta-catenin'], 'type': 'Individual_protein',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.e3', 'offsets': [[75, 87]],
+                'text': ['beta-catenin'], 'type': 'Individual_protein',
+                'normalized': []}]},
+            # Note that this isn't actually in the same doc, I changed the IDs
+            {'document_id': 'BioInfer.d0.s1', 'type': 'Sentence',
+            'text': 'A binary complex of birch profilin and skeletal muscle '
+            'actin could be isolated by gel chromatography.',
+            'entities': [{'id': 'BioInfer.d0.s1.e0', 'offsets': [[26, 34]],
+                'text': ['profilin'], 'type': 'Individual_protein',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s1.e1', 'offsets': [[39, 60]],
+                'text':['skeletal muscle actin'], 'type': 'Individual_protein',
+                'normalized': []}]}]
+
+@pytest.fixture
+def ent_anns_mult_doc_output():
+    return {
+            'BioInfer.d0.s0.e0': 'T1\tIndividual_protein 88 101\tT-cell factor',
+            'BioInfer.d0.s0.e1': 'T2\tIndividual_protein 0 13\talpha-catenin',
+            'BioInfer.d0.s0.e2': 'T3\tIndividual_protein 23 35\tbeta-catenin',
+            'BioInfer.d0.s0.e3': 'T4\tIndividual_protein 75 87\tbeta-catenin',
+            'BioInfer.d0.s1.e0': 'T5\tIndividual_protein 141 149\tprofilin',
+            'BioInfer.d0.s1.e1': 'T6\tIndividual_protein 154 175\tskeletal muscle actin'
+            }
+
+def test_get_ent_anns_mult_doc(ent_anns_mult_doc_input,
+        ent_anns_mult_doc_output):
+
+    ent_anns = hbb.get_ent_anns(ent_anns_mult_doc_input)
+
+    assert ent_anns == ent_anns_mult_doc_output
+
+
+@pytest.fixture
+def rel_anns_one_doc_input():
+    return [{'document_id': 'BioInfer.d0.s0',
+            'relations': [{'id': 'BioInfer.d0.s0.i0', 'type': 'PPI',
+                'arg1_id': 'BioInfer.d0.s0.e0', 'arg2_id': 'BioInfer.d0.s0.e1',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.i1', 'type': 'PPI',
+                'arg1_id': 'BioInfer.d0.s0.e0', 'arg2_id': 'BioInfer.d0.s0.e2',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.i2', 'type': 'PPI',
+                'arg1_id': 'BioInfer.d0.s0.e0', 'arg2_id': 'BioInfer.d0.s0.e3',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.i3', 'type': 'PPI',
+                'arg1_id': 'BioInfer.d0.s0.e1', 'arg2_id': 'BioInfer.d0.s0.e2',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.i4', 'type': 'PPI',
+                'arg1_id': 'BioInfer.d0.s0.e1', 'arg2_id': 'BioInfer.d0.s0.e3',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.i5', 'type': 'PPI',
+                'arg1_id': 'BioInfer.d0.s0.e2', 'arg2_id': 'BioInfer.d0.s0.e3',
+                'normalized': []}]}]
+
+@pytest.fixture
+def rel_anns_one_doc_output():
+    return {
+            'BioInfer.d0.s0.i0': 'R1\tPPI Arg1:T1 Arg2:T2',
+            'BioInfer.d0.s0.i1': 'R2\tPPI Arg1:T1 Arg2:T3',
+            'BioInfer.d0.s0.i2': 'R3\tPPI Arg1:T1 Arg2:T4',
+            'BioInfer.d0.s0.i3': 'R4\tPPI Arg1:T2 Arg2:T3',
+            'BioInfer.d0.s0.i4': 'R5\tPPI Arg1:T2 Arg2:T4',
+            'BioInfer.d0.s0.i5': 'R6\tPPI Arg1:T3 Arg2:T4'
+            }
+
+def test_get_rel_anns_one_doc(ent_anns_one_doc_output, rel_anns_one_doc_input,
+        rel_anns_one_doc_output):
+
+    rel_anns = hbb.get_rel_anns(rel_anns_one_doc_input,
+            ent_anns_one_doc_output)
+
+    assert rel_anns == rel_anns_one_doc_output
+
+
+@pytest.fixture
+def rel_anns_mult_doc_input():
+    return [{'document_id': 'BioInfer.d0.s0',
+            'relations': [{'id': 'BioInfer.d0.s0.i0', 'type': 'PPI',
+                'arg1_id': 'BioInfer.d0.s0.e0', 'arg2_id': 'BioInfer.d0.s0.e1',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.i1', 'type': 'PPI',
+                'arg1_id': 'BioInfer.d0.s0.e0', 'arg2_id': 'BioInfer.d0.s0.e2',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.i2', 'type': 'PPI',
+                'arg1_id': 'BioInfer.d0.s0.e0', 'arg2_id': 'BioInfer.d0.s0.e3',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.i3', 'type': 'PPI',
+                'arg1_id': 'BioInfer.d0.s0.e1', 'arg2_id': 'BioInfer.d0.s0.e2',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.i4', 'type': 'PPI',
+                'arg1_id': 'BioInfer.d0.s0.e1', 'arg2_id': 'BioInfer.d0.s0.e3',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.i5', 'type': 'PPI',
+                'arg1_id': 'BioInfer.d0.s0.e2', 'arg2_id': 'BioInfer.d0.s0.e3',
+                'normalized': []}]},
+            # Again note that I changed the IDs
+            {'document_id': 'BioInfer.d0.s1',
+            'relations': [{'id': 'BioInfer.d0.s1.i0', 'type': 'PPI',
+                'arg1_id': 'BioInfer.d0.s1.e0', 'arg2_id': 'BioInfer.d0.s1.e1',
+                'normalized': []}]}]
+
+@pytest.fixture
+def rel_anns_mult_doc_output():
+    return {
+            'BioInfer.d0.s0.i0': 'R1\tPPI Arg1:T1 Arg2:T2',
+            'BioInfer.d0.s0.i1': 'R2\tPPI Arg1:T1 Arg2:T3',
+            'BioInfer.d0.s0.i2': 'R3\tPPI Arg1:T1 Arg2:T4',
+            'BioInfer.d0.s0.i3': 'R4\tPPI Arg1:T2 Arg2:T3',
+            'BioInfer.d0.s0.i4': 'R5\tPPI Arg1:T2 Arg2:T4',
+            'BioInfer.d0.s0.i5': 'R6\tPPI Arg1:T3 Arg2:T4',
+            'BioInfer.d0.s1.i0': 'R7\tPPI Arg1:T5 Arg2:T6'
+            }
+
+def test_get_rel_anns_mult_doc(ent_anns_mult_doc_output,
+        rel_anns_mult_doc_input, rel_anns_mult_doc_output):
+
+    rel_anns = hbb.get_rel_anns(rel_anns_mult_doc_input,
+            ent_anns_mult_doc_output)
+
+    assert rel_anns == rel_anns_mult_doc_output
+
+
+@pytest.fixture
+def join_docs_one_doc_input():
+    return [{'document_id': 'BioInfer.d0.s0', 'type': 'Sentence',
+        'text': 'alpha-catenin inhibits beta-catenin signaling by preventing '
+        'formation of a beta-catenin*T-cell factor*DNA complex.',
+        'entities': [
+            {'id': 'BioInfer.d0.s0.e0', 'offsets': [[88, 101]],
+                'text': ['T-cell factor'], 'type': 'Individual_protein',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.e1', 'offsets': [[0, 13]],
+                'text': ['alpha-catenin'], 'type': 'Individual_protein',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.e2', 'offsets': [[23, 35]],
+                'text': ['beta-catenin'], 'type': 'Individual_protein',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.e3', 'offsets': [[75, 87]],
+                'text': ['beta-catenin'], 'type': 'Individual_protein',
+                'normalized': []}],
+        'relations': [
+            {'id': 'BioInfer.d0.s0.i0', 'type': 'PPI',
+                'arg1_id': 'BioInfer.d0.s0.e0', 'arg2_id': 'BioInfer.d0.s0.e1',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.i1', 'type': 'PPI',
+                'arg1_id': 'BioInfer.d0.s0.e0', 'arg2_id': 'BioInfer.d0.s0.e2',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.i2', 'type': 'PPI',
+                'arg1_id': 'BioInfer.d0.s0.e0', 'arg2_id': 'BioInfer.d0.s0.e3',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.i3', 'type': 'PPI',
+                'arg1_id': 'BioInfer.d0.s0.e1', 'arg2_id': 'BioInfer.d0.s0.e2',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.i4', 'type': 'PPI',
+                'arg1_id': 'BioInfer.d0.s0.e1', 'arg2_id': 'BioInfer.d0.s0.e3',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.i5', 'type': 'PPI',
+                'arg1_id': 'BioInfer.d0.s0.e2', 'arg2_id': 'BioInfer.d0.s0.e3',
+                'normalized': []}]}]
+
+@pytest.fixture
+def join_docs_one_doc_output(join_docs_one_doc_input):
+    joined_dset = {}
+    new_doc_key = 'BioInfer.d0'
+    joined_dset[new_doc_key] = join_docs_one_doc_input
+    return joined_dset
+
+def test_join_docs_one_doc(join_docs_one_doc_input, join_docs_one_doc_output):
+
+    joined_dset = hbb.join_docs(join_docs_one_doc_input)
+
+    assert joined_dset == join_docs_one_doc_output
+
+
+@pytest.fixture
+def join_docs_mult_doc_input():
+    return [{'document_id': 'BioInfer.d0.s0', 'type': 'Sentence',
+        'text': 'alpha-catenin inhibits beta-catenin signaling by preventing '
+        'formation of a beta-catenin*T-cell factor*DNA complex.',
+        'entities': [
+            {'id': 'BioInfer.d0.s0.e0', 'offsets': [[88, 101]],
+                'text': ['T-cell factor'], 'type': 'Individual_protein',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.e1', 'offsets': [[0, 13]],
+                'text': ['alpha-catenin'], 'type': 'Individual_protein',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.e2', 'offsets': [[23, 35]],
+                'text': ['beta-catenin'], 'type': 'Individual_protein',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.e3', 'offsets': [[75, 87]],
+                'text': ['beta-catenin'], 'type': 'Individual_protein',
+                'normalized': []}],
+        'relations': [
+            {'id': 'BioInfer.d0.s0.i0', 'type': 'PPI',
+                'arg1_id': 'BioInfer.d0.s0.e0', 'arg2_id': 'BioInfer.d0.s0.e1',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.i1', 'type': 'PPI',
+                'arg1_id': 'BioInfer.d0.s0.e0', 'arg2_id': 'BioInfer.d0.s0.e2',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.i2', 'type': 'PPI',
+                'arg1_id': 'BioInfer.d0.s0.e0', 'arg2_id': 'BioInfer.d0.s0.e3',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.i3', 'type': 'PPI',
+                'arg1_id': 'BioInfer.d0.s0.e1', 'arg2_id': 'BioInfer.d0.s0.e2',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.i4', 'type': 'PPI',
+                'arg1_id': 'BioInfer.d0.s0.e1', 'arg2_id': 'BioInfer.d0.s0.e3',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.i5', 'type': 'PPI',
+                'arg1_id': 'BioInfer.d0.s0.e2', 'arg2_id': 'BioInfer.d0.s0.e3',
+                'normalized': []}]},
+            # Changed IDs
+            {'document_id': 'BioInfer.d0.s1', 'type': 'Sentence',
+            'text': 'A binary complex of birch profilin and skeletal muscle '
+            'actin could be isolated by gel chromatography.',
+            'entities': [
+                {'id': 'BioInfer.d0.s1.e0', 'offsets': [[26, 34]],
+                    'text': ['profilin'], 'type': 'Individual_protein',
+                    'normalized': []},
+                {'id': 'BioInfer.d0.s1.e1', 'offsets': [[39, 60]],
+                    'text': ['skeletal muscle actin'], 'type': 'Individual_protein',
+                    'normalized': []}],
+            'relations': [
+                {'id': 'BioInfer.d0.s1.i0', 'type': 'PPI',
+                    'arg1_id': 'BioInfer.d0.s1.e0', 'arg2_id': 'BioInfer.d0.s1.e1',
+                    'normalized': []}]}]
+
+@pytest.fixture
+def join_docs_mult_doc_output(join_docs_mult_doc_input):
+    joined_dset = {}
+    new_doc_key = 'BioInfer.d0'
+    joined_dset[new_doc_key] = join_docs_mult_doc_input
+    return joined_dset
+
+def test_join_docs_mult_doc(join_docs_mult_doc_input,
+        join_docs_mult_doc_output):
+
+    joined_dset = hbb.join_docs(join_docs_mult_doc_input)
+
+    assert joined_dset == join_docs_mult_doc_output
+
+
+@pytest.fixture
+def join_docs_unique_input():
+    return [{'document_id': 'BioInfer.d0.s0', 'type': 'Sentence',
+        'text': 'alpha-catenin inhibits beta-catenin signaling by preventing '
+        'formation of a beta-catenin*T-cell factor*DNA complex.',
+        'entities': [
+            {'id': 'BioInfer.d0.s0.e0', 'offsets': [[88, 101]],
+                'text': ['T-cell factor'], 'type': 'Individual_protein',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.e1', 'offsets': [[0, 13]],
+                'text': ['alpha-catenin'], 'type': 'Individual_protein',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.e2', 'offsets': [[23, 35]],
+                'text': ['beta-catenin'], 'type': 'Individual_protein',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.e3', 'offsets': [[75, 87]],
+                'text': ['beta-catenin'], 'type': 'Individual_protein',
+                'normalized': []}],
+        'relations': [
+            {'id': 'BioInfer.d0.s0.i0', 'type': 'PPI',
+                'arg1_id': 'BioInfer.d0.s0.e0', 'arg2_id': 'BioInfer.d0.s0.e1',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.i1', 'type': 'PPI',
+                'arg1_id': 'BioInfer.d0.s0.e0', 'arg2_id': 'BioInfer.d0.s0.e2',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.i2', 'type': 'PPI',
+                'arg1_id': 'BioInfer.d0.s0.e0', 'arg2_id': 'BioInfer.d0.s0.e3',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.i3', 'type': 'PPI',
+                'arg1_id': 'BioInfer.d0.s0.e1', 'arg2_id': 'BioInfer.d0.s0.e2',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.i4', 'type': 'PPI',
+                'arg1_id': 'BioInfer.d0.s0.e1', 'arg2_id': 'BioInfer.d0.s0.e3',
+                'normalized': []},
+            {'id': 'BioInfer.d0.s0.i5', 'type': 'PPI',
+                'arg1_id': 'BioInfer.d0.s0.e2', 'arg2_id': 'BioInfer.d0.s0.e3',
+                'normalized': []}]},
+            {'document_id': 'BioInfer.d1.s0', 'type': 'Sentence',
+            'text': 'A binary complex of birch profilin and skeletal muscle '
+            'actin could be isolated by gel chromatography.',
+            'entities': [
+                {'id': 'BioInfer.d1.s0.e0', 'offsets': [[26, 34]],
+                    'text': ['profilin'], 'type': 'Individual_protein',
+                    'normalized': []},
+                {'id': 'BioInfer.d1.s0.e1', 'offsets': [[39, 60]],
+                    'text': ['skeletal muscle actin'], 'type': 'Individual_protein',
+                    'normalized': []}],
+            'relations': [
+                {'id': 'BioInfer.d1.s0.i0', 'type': 'PPI',
+                    'arg1_id': 'BioInfer.d1.s0.e0', 'arg2_id': 'BioInfer.d1.s0.e1',
+                    'normalized': []}]}]
+
+@pytest.fixture
+def join_docs_unique_output(join_docs_unique_input):
+    joined_dset = {}
+    joined_dset['BioInfer.d0'] = [join_docs_unique_input[0]]
+    joined_dset['BioInfer.d1'] = [join_docs_unique_input[1]]
+    return joined_dset
+
+def test_join_docs_unique(join_docs_unique_input,
+        join_docs_unique_output):
+
+    joined_dset = hbb.join_docs(join_docs_unique_input)
+
+    assert joined_dset == join_docs_unique_output
+
